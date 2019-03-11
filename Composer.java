dce5c4bdabb5aed2cc6eaea7aaf2821e50d59baf
danieltmc <me@danieltmc.com>
2019-03-11 13:24:26 -0400

    Basic constructor added

---
+++ Composer.java
@@ -0,0 +1,20 @@
+class Composer
+{
+int tempo;
+int parts;
+int length;
+int beats;
+
+	private Composer() // Default constructor for proof of concept
+	{
+		this.tempo = 120;	// Default to 120 bpm
+		this.parts = 4;		// Default to 4 parts (SATB)
+		this.length = 3;	// Default to 3 minutes
+		this.beats = 120 * 3;	// Total number of beats in the song
+	}
+
+	public static void main(Sring[] args)
+	{
+		return;
+	}
+}
